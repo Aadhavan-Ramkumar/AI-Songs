efa2589aa5abf2e1de6ec90a77bbe5aeb71955ba
Aadhavan-Ramkumar <74062499+Aadhavan-Ramkumar@users.noreply.github.com>
2022-06-22 20:54:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var Song1, Song2
 var Song1Name = "Girls Like You",
-    Song2Name = "Enemy"
+    Song2Name = "Memories"
 var LeftWristX = 0,
     LeftWristY = 0,
     RightWristX = 0,
     RightWristY = 0
-var LeftWristScore = 0
-var Song1Playing = ""
+var LeftWristScore = 0,
+    RightWristScore = 0
+var Song1Playing = "",
+    Song2Playing = ""
 
 function preload() {
     Song1 = loadSound("Girls Like You.mp3")
-    Song2 = loadSound("Enemy.mp3")
+    Song2 = loadSound("Memories.mp3")
 }
 
 function setup() {
@@ -34,7 +36,7 @@ function draw() {
     stroke("red")
     fill("red")
 
-    if (LeftWristScore > 0.002) {
+    if (LeftWristScore > 0.2) {
         circle(LeftWristX, LeftWristY, 20)
         Song2.stop()
         if (Song1Playing == false) {
@@ -42,11 +44,21 @@ function draw() {
             document.getElementById("SongName").innerHTML = "Song Name: " + Song1Name
         }
     }
+
+    if (RightWristScore > 0.2) {
+        circle(RightWristX, RightWristY, 20)
+        Song1.stop()
+        if (Song2Playing == false) {
+            Song2.play()
+            document.getElementById("SongName").innerHTML = "Song Name: " + Song2Name
+        }
+    }
 }
 
 function GetPoses(Results) {
     if (Results.length > 0) {
         LeftWristScore = Results[0].pose.keypoints[9].score
+        RightWristScore = Results[0].pose.keypoints[10].score
 
         LeftWristX = Results[0].pose.leftWrist.x
         LeftWristY = Results[0].pose.leftWrist.y
